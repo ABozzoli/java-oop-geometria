8f407363883867fcbc5aaef3064d83608c79eb04
ABozzoli <alessandrobozzoli92@gmail.com>
2021-11-23 15:43:29 +0100

    Classe Rettangolo con attr. base e altez. + costruttore

---
+++ src/org/generation/italy/geometria/Rettangolo.java
@@ -0,0 +1,15 @@
+package org.generation.italy.geometria;
+
+public class Rettangolo {
+
+	// attributi
+	int base;
+	int altezza;
+	
+	// costruttori
+	Rettangolo(int base, int altezza){
+		this.base = base;
+		this.altezza = altezza;
+	}
+	
+}
